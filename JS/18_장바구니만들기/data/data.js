966b50c7c40bfe738112a6c34005fe944889e31d
Hyeongjong Kim <ocjjiya@gmail.com>
2023-04-27 00:35:41 +0900

    home Apr27

--- JS/18_장바구니만들기/data/data.js
+++ JS/18_장바구니만들기/data/data.js
@@ -2,10 +2,10 @@
 
 let data = [
     { id: 0, title: '고양이 1', imgSrc: 'images/cat0.jpg', state: 'onlist' },
-    { id: 1, title: '고양이 2', imgSrc: 'images/cat1.jpg', state: 'del' },
-    { id: 2, title: '고양이 3', imgSrc: 'images/cat2.jpg', state: 'onbasket' },
+    { id: 1, title: '고양이 2', imgSrc: 'images/cat1.jpg', state: 'onlist' },
+    { id: 2, title: '고양이 3', imgSrc: 'images/cat2.jpg', state: 'onlist' },
     { id: 3, title: '고양이 4', imgSrc: 'images/cat3.jpg', state: 'onlist' },
-    { id: 4, title: '고양이 5', imgSrc: 'images/cat4.jpg', state: 'onbasket' },
+    { id: 4, title: '고양이 5', imgSrc: 'images/cat4.jpg', state: 'onlist' },
     { id: 5, title: '고양이 6', imgSrc: 'images/cat5.jpg', state: 'onlist' },
 ];
 
