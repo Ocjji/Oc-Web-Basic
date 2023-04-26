af94e3b11f37534b1f6dc6a86aecab76abb3ddc6
Hyeongjong Kim <ocjjiya@gmail.com>
2023-04-27 00:34:49 +0900

    home Apr27

--- JS/18_장바구니만들기/js/main.js
+++ JS/18_장바구니만들기/js/main.js
@@ -6,29 +6,35 @@ console.log(data);
 let listItem = document.querySelector('.listItem');
 let pocket = document.querySelector('.pocket');
 let removeAll = document.querySelector('.removeAll');
-let reset = document.querySelector('.removeAll');
+let reset = document.querySelector('.reset');
 
-let btn_add = null;
-
-let btn_del = null;
-let btn_recover = null;
+let btn_add = [];
+let btn_del = [];
+let btn_recover = [];
+let listItem_li = [];
+let pocket_li = [];
 
 function happy() {
     listItem.innerHTML = null;
     pocket.innerHTML = null;
+
     for (let i = 0; i < data.length; i++) {
         if (data[i].state === 'onlist') {
             let li = document.createElement('li');
+
             let div = document.createElement('div');
             div.dataset.id = data[i].id;
-            console.log(div);
+
             let img = document.createElement('img');
             img.setAttribute('src', data[i].imgSrc);
+
             let strong = document.createElement('strong');
             strong.textContent = data[i].title;
+
             let btn1 = document.createElement('button');
             btn1.textContent = '담기';
             btn1.classList.add('add');
+
             let btn2 = document.createElement('button');
             btn2.textContent = '삭제';
             btn2.classList.add('del');
@@ -37,20 +43,25 @@ function happy() {
             li.append(div);
             listItem.append(li);
 
+            listItem_li = document.querySelectorAll('.listItem li');
             btn_add = document.querySelectorAll('.add');
             btn_del = document.querySelectorAll('.del');
         } else if (data[i].state === 'onbasket') {
             let li = document.createElement('li');
+
             let div = document.createElement('div');
             div.dataset.id = data[i].id;
-            div.classList.add(data[i].id)
+
             let img = document.createElement('img');
             img.setAttribute('src', data[i].imgSrc);
+
             let strong = document.createElement('strong');
             strong.textContent = data[i].title;
+
             let btn1 = document.createElement('button');
             btn1.textContent = '복구';
             btn1.classList.add('recover');
+
             let btn2 = document.createElement('button');
             btn2.textContent = '삭제';
             btn2.classList.add('del');
@@ -58,12 +69,13 @@ function happy() {
             div.append(img, strong, btn1, btn2);
             li.append(div);
             pocket.append(li);
+
+            pocket_li = document.querySelectorAll('.pocket li');
             btn_recover = document.querySelectorAll('.recover');
             btn_del = document.querySelectorAll('.del');
-        } else if (data[i].state === 'del') {
-            // 구현 안함
         }
     }
+
     btn_add.forEach(element => {
         element.addEventListener('click', e => {
             // console.log(e.currentTarget.parentElement.dataset.id);
@@ -72,52 +84,43 @@ function happy() {
             happy();
         })
     })
+
     btn_recover.forEach(element => {
         element.addEventListener('click', e => {
-            console.log(e.currentTarget.parentElement.dataset.id);
+            // console.log(e.currentTarget.parentElement.dataset.id);
             data[e.currentTarget.parentElement.dataset.id].state = 'onlist';
 
             happy();
         })
     })
+
     btn_del.forEach(element => {
         element.addEventListener('click', e => {
-            console.log(e.currentTarget.parentElement.dataset.id);
+            // console.log(e.currentTarget.parentElement.dataset.id);
             data[e.currentTarget.parentElement.dataset.id].state = 'del';
-
             happy();
         })
     })
-
 }
 
-happy();
 removeAll.addEventListener('click', e => {
-    console.log(e.currentTarget.nextSibling);
+    pocket_li.forEach(element => {
+        // console.log(element.children[0]);
+        if (data[element.children[0].dataset.id].state === 'onbasket') {
+            data[element.children[0].dataset.id].state = 'del';
+        }
+    });
+    happy();
 })
 
-/* 
-btn_add.forEach(element => {
-    element.addEventListener('click', e => {
-        // console.log(e.currentTarget.parentElement.dataset.id);
-        data[e.currentTarget.parentElement.dataset.id].state = 'onbasket';
-
-        happy();
-    })
+reset.addEventListener('click', e => {
+    pocket_li.forEach(element => {
+        // console.log(element.children[0]);
+        if (data[element.children[0].dataset.id].state === 'onbasket') {
+            data[element.children[0].dataset.id].state = 'onlist';
+        }
+    });
+    happy();
 })
-btn_recover.forEach(element => {
-    element.addEventListener('click', e => {
-        console.log(e.currentTarget.parentElement.dataset.id);
-        data[e.currentTarget.parentElement.dataset.id].state = 'onlist';
 
-        happy();
-    })
-}) */
-
-
-
-
-
-
-
-
+happy();
